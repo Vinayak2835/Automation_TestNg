4eeda982b91e772eebc4e71d88d6bc082cf5cd1f
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-07-27 17:25:58 +0530

    [Vinayak] Add: Lanching different browsers using TestNG

--- src/test/java/com/salenium/LaunchFireBoxAndChromeTogether.java
+++ src/test/java/com/salenium/LaunchFireBoxAndChromeTogether.java
@@ -8,7 +8,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class LaunchFireBoxAndChromeTogether {
+public  class LaunchFireBoxAndChromeTogether {
 	
 	WebDriver driver = null;
 	
